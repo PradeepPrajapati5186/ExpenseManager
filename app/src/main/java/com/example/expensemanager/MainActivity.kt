00b710d40eae5dc5cd530bdb0b10de6f3ac4fa58
PradeepPrajapati5186 <pp136859@gmail.com>
2025-05-13 18:59:43 +0530

    Added login Page

--- app/src/main/java/com/example/expensemanager/MainActivity.kt
+++ app/src/main/java/com/example/expensemanager/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.expensemanager.ui.screens.auth.LoginScreen
 import com.example.expensemanager.ui.theme.ExpenseManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +18,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExpenseManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LoginScreen(modifier= Modifier.padding(innerPadding))
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExpenseManagerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/expensemanager/ui/screens/auth/LoginScreen.kt
@@ -0,0 +1,56 @@
+
+package com.example.expensemanager.ui.screens.auth
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+
+fun LoginScreen(modifier: Modifier) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(text="LOGIN PAGE")
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = email,
+            onValueChange = {email = it },
+            label = {Text(text = "Email") })
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = password,
+            onValueChange = {password = it },
+            label = {Text(text = "Password") })
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {}) {
+            Text(text = "Login")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        TextButton(onClick = {}) {
+            Text(text = "Want to Register?CLick Here")
+        }
+
+
+
+    }
+}
+
